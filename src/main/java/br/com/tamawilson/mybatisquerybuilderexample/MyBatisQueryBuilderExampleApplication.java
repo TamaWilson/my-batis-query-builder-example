7cd1bca35d5072fb10cbf0238217ae910bee0ace
TamaWilson <wilson_tama@hotmail.com>
2021-02-07 15:46:55 -0300

    feat(Main): Main class

---
+++ src/main/java/br/com/tamawilson/mybatisquerybuilderexample/MyBatisQueryBuilderExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.tamawilson.mybatisquerybuilderexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisQueryBuilderExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisQueryBuilderExampleApplication.class, args);
+    }
+
+}
